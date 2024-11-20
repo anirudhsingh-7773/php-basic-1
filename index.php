670a02db7022ca934fa9d108a7aba4c280f99932
Anirudh Singh <anirudh.singh.7773@gmail.com>
2024-11-20 14:11:05 +0530

    task 2 first commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP task</title>
+</head>
+
+<body>
+
+  <?php require 'task1.php'; ?>
+
+  <h1>PHP Form</h1>
+  <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" enctype="multipart/form-data">
+    <label for="fname">First Name: </label>
+    <input type="text" name="fname">
+    <span class="error" style="color: red;">* <?php echo $formdata->fnameErr; ?></span>
+    <br><br>
+    <label for="lname">Last Name: </label>
+    <input type="text" name="lname">
+    <span class="error" style="color: red;">* <?php echo $formdata->lnameErr; ?></span>
+    <br><br>
+    <label for="name">Full Name: </label>
+    <input type="text" name="name" value="<?php echo $formdata->name; ?>" disabled>
+    <br><br>
+    <label for="fileToUpload">Select some image:</label>
+    <input type="file" id="fileToUpload" name="fileToUpload" />
+    <br><br>
+    <input type="submit" name="submit" id="submit">
+  </form>
+  <br>
+  
+  
+  <?php require 'upload.php' ?>
+  <?php if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
+    $fileobject->validate();
+    echo '<img src="/uploads/' . $_FILES["fileToUpload"]["name"] . '" alt="image" height = "300" width = "300">';
+  } ?>
+  <?php echo $formdata->greetings(); ?>
+</body>
+
+</html>
