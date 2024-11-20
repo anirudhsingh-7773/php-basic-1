ed27d8e641ffb6e1eaf8eb86b595025ca143ec58
Anirudh Singh <anirudh.singh.7773@gmail.com>
2024-11-20 14:32:41 +0530

    remove commented code

--- upload.php
+++ upload.php
@@ -14,19 +14,12 @@ class fileUploads
     if (isset($_POST["submit"])) {
       $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
       if ($check !== false) {
-        // echo "File is an image - " . $check["mime"] . ".<br>";
         $uploadOk = 1;
       } else {
         echo "File is not an image.<br>";
         $uploadOk = 0;
       }
     }
-
-    // Check if file already exists
-    // if (file_exists($target_file)) {
-    //   echo "Sorry, file already exists.";
-    //   $uploadOk = 0;
-    // }
 
     // Check file size
     if ($_FILES["fileToUpload"]["size"] > 5000000) {
@@ -49,7 +42,7 @@ class fileUploads
       // if everything is ok, try to upload file
     } else {
       if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        // echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.<br>";
+        
       } else {
         echo "Sorry, there was an error uploading your file.<br>";
       }
