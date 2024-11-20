e244b3d282ce0670c0f97b60bc4607c04d165de2
Anirudh Singh <anirudh.singh.7773@gmail.com>
2024-11-20 17:40:36 +0530

    task 3 first commit

--- index.php
+++ index.php
@@ -27,17 +27,24 @@
     <label for="fileToUpload">Select some image:</label>
     <input type="file" id="fileToUpload" name="fileToUpload" />
     <br><br>
+    <label for="marks">Subject Marks (Subject|marks) :</label>
+    <textarea name="marks" id="marks" rows="5" cols="25"></textarea>
+    <br><br>
     <input type="submit" name="submit" id="submit">
   </form>
   <br>
-  
-  
-  <?php require 'upload.php' ?>
-  <?php if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
+
+
+  <?php
+  require 'upload.php';
+  $formdata->marksValidate();
+  if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
     $fileobject->validate();
     echo '<img src="/uploads/' . $_FILES["fileToUpload"]["name"] . '" alt="image" height = "300" width = "300">';
-  } ?>
-  <?php echo $formdata->greetings(); ?>
+    echo $formdata->greetings();
+    $formdata->displayMarksTable();
+  }
+  ?>
 </body>
 
 </html>
