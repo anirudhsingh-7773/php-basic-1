688e7fe0f1214bebbbdb54732f0884272bad2882
Anirudh Singh <anirudh.singh.7773@gmail.com>
2024-11-25 11:26:39 +0530

    Corrections in handling the form

--- index.php
+++ index.php
@@ -1,35 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" href="/favicon.ico">
-  <title>Task 1 PHP</title>
+  <title>PHP Task 1</title>
 </head>
-
 <body>
 
-  <?php include 'task1.php'; ?>
-
-  <h1>PHP Form</h1>
-  <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+<h1>Fill the Form</h1>
+<form method="post" action="form.php">
     <label for="fname">First Name: </label>
-    <input type="text" name="fname">
-    <span class="error" style="color: red;">* <?php echo $formdata->fnameErr; ?></span>
+    <input type="text" name="fname" id="fname" oninput="autofill('fname','lname','fullname')" required>
+    <span class="error" style="color: red;">* </span>
     <br><br>
     <label for="lname">Last Name: </label>
-    <input type="text" name="lname">
-    <span class="error" style="color: red;">* <?php echo $formdata->lnameErr; ?></span>
+    <input type="text" name="lname" id="lname" oninput="autofill('fname','lname','fullname')" required>
+    <span class="error" style="color: red;">* </span>
     <br><br>
     <label for="name">Full Name: </label>
-    <input type="text" name="name" value="<?php echo $formdata->name; ?>" disabled>
+    <input type="text" name="name" id="fullname" value="" disabled>
     <br><br>
     <input type="submit" name="submit" id="submit">
   </form>
 
-  <?php echo $formdata->greetings(); ?>
-
+  <script src="/js/script.js"></script>
 </body>
-
 </html>
