a709fb9dec17ede514781b493bf2cbc96809cb07
Anirudh Singh <anirudh.singh.7773@gmail.com>
2024-11-25 13:17:00 +0530

    correction in form handling

--- index.php
+++ index.php
@@ -1,55 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PHP task</title>
+  <title>PHP Task 2</title>
 </head>
-
 <body>
 
-  <?php require 'task1.php'; ?>
-
-  <h1>PHP Form</h1>
-  <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" enctype="multipart/form-data">
+<h1>Fill the Form</h1>
+<form method="post" action="form.php" enctype="multipart/form-data">
     <label for="fname">First Name: </label>
-    <input type="text" name="fname">
-    <span class="error" style="color: red;">* <?php echo $formdata->fnameErr; ?></span>
+    <input type="text" name="fname" id="fname" oninput="autofill('fname','lname','fullname')" required>
+    <span class="error" style="color: red;">* </span>
     <br><br>
     <label for="lname">Last Name: </label>
-    <input type="text" name="lname">
-    <span class="error" style="color: red;">* <?php echo $formdata->lnameErr; ?></span>
+    <input type="text" name="lname" id="lname" oninput="autofill('fname','lname','fullname')" required>
+    <span class="error" style="color: red;">* </span>
     <br><br>
     <label for="name">Full Name: </label>
-    <input type="text" name="name" value="<?php echo $formdata->name; ?>" disabled>
+    <input type="text" name="name" id="fullname" value="" disabled>
     <br><br>
-    <label for="fileToUpload">Select some image:</label>
-    <input type="file" id="fileToUpload" name="fileToUpload" />
+    <label for="image">Choose an image:</label>
+    <input type="file" id="image" name="uploadImage">
     <br><br>
     <label for="marks">Subject Marks (Subject|marks) :</label>
     <textarea name="marks" id="marks" rows="5" cols="25"></textarea>
     <br><br>
     <label for="phone">Phone (+91 xxxxxxxxxx): </label>
     <input type="text" id="phone" name="phone">
-    <span class="error" style="color: red;">* <?php echo $formdata->phoneErr; ?></span>
     <br><br>
     <input type="submit" name="submit" id="submit">
   </form>
-  <br>
 
-
-  <?php
-  require 'upload.php';
-  $formdata->marksValidate();
-  if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
-    $fileobject->validate();
-    echo '<img src="/uploads/' . $_FILES["fileToUpload"]["name"] . '" alt="image" height = "300" width = "300">';
-    echo $formdata->greetings();
-    $formdata->phoneOutput();
-    $formdata->displayMarksTable();
-  }
-  ?>
+  <script src="/js/script.js"></script>
 </body>
-
 </html>
