39a5cba1a612f204d88035ec062efae2461ef2e2
Anirudh Singh <anirudh.singh.7773@gmail.com>
2024-11-25 12:43:57 +0530

    Corrections in output display

---
+++ form.php
@@ -0,0 +1,75 @@
+<?php
+class form
+{
+
+  public $fname;
+  public $lname;
+  public $fullname;
+  public $content;
+
+  public function __construct()
+  {
+    if (empty($_POST['fname'])) {
+      echo "Enter First Name<br>";
+    } else if (!preg_match('/^[a-zA-Z]+$/', $_POST['fname'])) {
+      echo "First name can only contain letters!<br>";
+    } else {
+      $this->fname = $this->test_input($_POST["fname"]);
+    }
+
+    if (empty($_POST['lname'])) {
+      echo "Enter Last Name<br>";
+    } else if (!preg_match('/^[a-zA-Z]+$/', $_POST['lname'])) {
+      echo "Last name can only contain letters!<br>";
+    } else {
+      $this->lname = $this->test_input($_POST["lname"]);
+    }
+
+    if (!empty($this->fname) && !empty($this->lname)) {
+      $this->fullname = $this->fname . ' ' . $this->lname;
+      echo '<h1>Hello ' . $this->fullname . '!</h1><br>';
+    }
+  }
+
+  function test_input($data)
+  {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+
+  public function imageValidation()
+  {
+    if (!isset($_FILES["uploadImage"])) {
+      echo "No file uploaded.<br>";
+      return;
+    }
+    $target_dir = __DIR__ . '/uploads/';
+    $target_file = $target_dir . basename($_FILES["uploadImage"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+    umask(0);
+
+    // Allow certain file formats
+    if (!in_array($imageFileType, ["jpg", "jpeg", "png", "gif"])) {
+      echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.<br>";
+      $uploadOk = 0;
+    }
+
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+      echo "Sorry, your file was not uploaded.<br>";
+    } else {
+      // Attempt to upload the file
+      if (move_uploaded_file($_FILES["uploadImage"]["tmp_name"], $target_file)) {
+        echo '<img width="300" height="300" src="uploads/'.htmlspecialchars($_FILES["uploadImage"]["name"]).'"/>';
+      } else {
+        echo "Sorry, there was an error uploading your file.<br>";
+      }
+    }
+  }
+}
+
+$formdata = new form();
+$formdata->imageValidation();
