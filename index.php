1e8dba45df80941a3a203f5aeede4f4183d85bbf
Anirudh Singh <anirudh.singh.7773@gmail.com>
2024-11-26 11:02:42 +0530

    Change title

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PHP Task 2</title>
+  <title>PHP Task 4</title>
 </head>
 <body>
 
